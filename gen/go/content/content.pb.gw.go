101ac9a1dc91f024848918c866947970a2be4d66
your Name <qwertyshurazh@gmail.com>
2024-07-14 21:14:57 +0300

    fix delete direction

--- gen/go/content/content.pb.gw.go
+++ gen/go/content/content.pb.gw.go
@@ -153,19 +153,25 @@ func local_request_Content_CreateDirection_0(ctx context.Context, marshaler runt
 
 }
 
-var (
-	filter_Content_DeleteDirection_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
-)
-
 func request_Content_DeleteDirection_0(ctx context.Context, marshaler runtime.Marshaler, client ContentClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq DeleteDirectionRequest
 	var metadata runtime.ServerMetadata
 
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["directionId"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "directionId")
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Content_DeleteDirection_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+
+	protoReq.DirectionId, err = runtime.Int64(val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "directionId", err)
 	}
 
 	msg, err := client.DeleteDirection(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
@@ -177,11 +183,21 @@ func local_request_Content_DeleteDirection_0(ctx context.Context, marshaler runt
 	var protoReq DeleteDirectionRequest
 	var metadata runtime.ServerMetadata
 
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["directionId"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "directionId")
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Content_DeleteDirection_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+
+	protoReq.DirectionId, err = runtime.Int64(val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "directionId", err)
 	}
 
 	msg, err := server.DeleteDirection(ctx, &protoReq)
@@ -303,7 +319,7 @@ func RegisterContentHandlerServer(ctx context.Context, mux *runtime.ServeMux, se
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
 		var err error
 		var annotatedContext context.Context
-		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/content.Content/DeleteDirection", runtime.WithHTTPPathPattern("/direction"))
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/content.Content/DeleteDirection", runtime.WithHTTPPathPattern("/direction/{directionId}"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -455,7 +471,7 @@ func RegisterContentHandlerClient(ctx context.Context, mux *runtime.ServeMux, cl
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
 		var err error
 		var annotatedContext context.Context
-		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/content.Content/DeleteDirection", runtime.WithHTTPPathPattern("/direction"))
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/content.Content/DeleteDirection", runtime.WithHTTPPathPattern("/direction/{directionId}"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -483,7 +499,7 @@ var (
 
 	pattern_Content_CreateDirection_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"direction"}, ""))
 
-	pattern_Content_DeleteDirection_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"direction"}, ""))
+	pattern_Content_DeleteDirection_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1}, []string{"direction", "directionId"}, ""))
 )
 
 var (
